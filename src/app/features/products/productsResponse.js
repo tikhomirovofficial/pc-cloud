81cf7ca03550ee12db335bfbfa68203bd1b1a5a2
tikhomirovofficial <arteom.tixomirow@yandex.ru>
2024-05-18 23:37:18 +0300

    filled all products

--- src/app/features/products/productsResponse.js
+++ src/app/features/products/productsResponse.js
@@ -73,6 +73,366 @@ export const productsResponse = {
             sale_price: 89990,
             rating: 4.1,
             image: getImagePath("computers/8.webp")
+        },
+        {
+            id: 9,
+            category: 1,
+            name: "ПК ARDOR GAMING NEO M137",
+            price: 66990,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("computers/9.webp")
+        },
+        {
+            id: 10,
+            category: 1,
+            name: "ПК MSI MAG Infinite S3 13NUD-871XRU",
+            price: 116990,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("computers/10.webp")
+        },
+        {
+            id: 11,
+            category: 1,
+            name: "ПК ARDOR GAMING NEO M144",
+            price: 56990,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("computers/11.webp")
+        },
+        {
+            id: 12,
+            category: 1,
+            name: "ПК DEXP Atlas H393",
+            price: 36990,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("computers/12.webp")
+        },
+        {
+            id: 13,
+            category: 2,
+            name: "6.1 Смартфон Apple iPhone 15",
+            price: 86990,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("phones/1.webp")
+        },
+        {
+            id: 14,
+            category: 2,
+            name: "6.7 Смартфон Apple iPhone 15 Pro Max",
+            price: 146990,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("phones/2.webp")
+        },
+        {
+            id: 15,
+            category: 2,
+            name: "6.1 Смартфон Apple iPhone 15 Pro",
+            price: 136990,
+            sale_price: null,
+            rating: 4.2,
+            image: getImagePath("phones/3.webp")
+        },
+        {
+            id: 16,
+            category: 2,
+            name: "6.1 Смартфон Apple iPhone 13",
+            price: 66990,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("phones/4.webp")
+        },
+        {
+            id: 17,
+            category: 2,
+            name: "6.67 Смартфон Xiaomi Redmi Note 12",
+            price: 15990,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("phones/5.webp")
+        },
+        {
+            id: 18,
+            category: 2,
+            name: "6.79 Смартфон Xiaomi Redmi 12",
+            price: 16990,
+            sale_price: null,
+            rating: 4.1,
+            image: getImagePath("phones/6.webp")
+        },
+        {
+            id: 19,
+            category: 2,
+            name: "6.8 Смартфон Samsung Galaxy S24 Ultra",
+            price: 159990,
+            sale_price: 149990,
+            rating: 4.3,
+            image: getImagePath("phones/7.webp")
+        },
+        {
+            id: 20,
+            category: 2,
+            name: "6.8 Смартфон HONOR Magic6 Pro",
+            price: 119990,
+            sale_price: 109990,
+            rating: 4.5,
+            image: getImagePath("phones/8.webp")
+        },
+        {
+            id: 21,
+            category: 2,
+            name: "6.78 Смартфон Infinix NOTE 30",
+            price: 13990,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("phones/9.webp")
+        },
+        {
+            id: 22,
+            category: 2,
+            name: "6.4 Смартфон Samsung Galaxy S21 FE",
+            price: 49990,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("phones/10.webp")
+        },
+        {
+            id: 23,
+            category: 2,
+            name: "6.6 Смартфон Samsung Galaxy A55",
+            price: 47990,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("phones/11.webp")
+        },
+        {
+            id: 24,
+            category: 2,
+            name: "6.67 Смартфон Xiaomi 13T",
+            price: 45990,
+            sale_price: null,
+            rating: 4.7,
+            image: getImagePath("phones/12.webp")
+        },
+        {
+            id: 25,
+            category: 3,
+            name: "Видеокарта KFA2 GeForce RTX 4060",
+            price: 33990,
+            sale_price: null,
+            rating: 4.7,
+            image: getImagePath("accessories/1.webp")
+        },
+        {
+            id: 26,
+            category: 3,
+            name: "Видеокарта MSI GeForce RTX 4070 VENTUS",
+            price: 66990,
+            sale_price: null,
+            rating: 4.8,
+            image: getImagePath("accessories/2.webp")
+        },
+        {
+            id: 27,
+            category: 3,
+            name: "Видеокарта KFA2 GeForce RTX 4060 X Black",
+            price: 34990,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("accessories/3.webp")
+        },
+        {
+            id: 28,
+            category: 3,
+            name: "Видеокарта KFA2 GeForce GTX 1650 X Black",
+            price: 14990,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("accessories/4.webp")
+        },
+        {
+            id: 29,
+            category: 3,
+            name: "Процессор Intel Core i5-12400F OEM 6 x 2.5 ГГц",
+            price: 13990,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("accessories/5.webp")
+        },
+        {
+            id: 30,
+            category: 3,
+            name: "Процессор AMD Ryzen 5 5600 OEM 6 x 3.5 ГГц",
+            price: 11990,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("accessories/6.webp")
+        },
+        {
+            id: 31,
+            category: 3,
+            name: "Процессор AMD Ryzen 5 7500F OEM 6 x 3.7 ГГц",
+            price: 18990,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("accessories/7.webp")
+        },
+        {
+            id: 32,
+            category: 3,
+            name: "Процессор Intel Core i5-13400F OEM 6 x 2.5 ГГц",
+            price: 19990,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("accessories/8.webp")
+        },
+        {
+            id: 33,
+            category: 3,
+            name: "Оперативная память Kingston FURY Beast Black DDR4 16 ГБx2",
+            price: 8790,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("accessories/9.webp")
+        },
+        {
+            id: 34,
+            category: 3,
+            name: "Оперативная память ADATA XPG Lancer Blade DDR5 16 ГБx2",
+            price: 10790,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("accessories/10.webp")
+        },
+        {
+            id: 35,
+            category: 3,
+            name: "Оперативная память ADATA XPG GAMMIX D20 DDR4 8 ГБx2",
+            price: 3790,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("accessories/11.webp")
+        },
+        {
+            id: 36,
+            category: 3,
+            name: "Оперативная память ADATA XPG Lancer DDR5 16 ГБx2",
+            price: 11790,
+            sale_price: null,
+            rating: 4.1,
+            image: getImagePath("accessories/12.webp")
+        },
+        {
+            id: 37,
+            category: 4,
+            name: "23.8 Монитор ARDOR GAMING PORTAL AF24H1 1920x1080 (FullHD)@144 Гц",
+            price: 13490,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("periphery/1.webp")
+        },
+        {
+            id: 38,
+            category: 4,
+            name: "27 Монитор MSI G274QPF черный 2560x1440@170 Гц",
+            price: 28490,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("periphery/2.webp")
+        },
+        {
+            id: 39,
+            category: 4,
+            name: "27 Монитор ARDOR GAMING INFINITY PRO AQ27H1 2560x1440@165 Гц",
+            price: 24490,
+            sale_price: 19990,
+            rating: 4.8,
+            image: getImagePath("periphery/3.webp")
+        },
+        {
+            id: 40,
+            category: 4,
+            name: "27 Монитор AOC Q27G2S/EU черный 2560x1440@165 Гц",
+            price: 32490,
+            sale_price: 28990,
+            rating: 4.7,
+            image: getImagePath("periphery/4.webp")
+        },
+        {
+            id: 41,
+            category: 4,
+            name: "Клавиатура проводная ARDOR GAMING Blade",
+            price: 4490,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("periphery/5.webp")
+        },
+        {
+            id: 42,
+            category: 4,
+            name: "Клавиатура проводная Dark Project KD87A Gateron Teal Cap",
+            price: 10490,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("periphery/6.webp")
+        },
+        {
+            id: 43,
+            category: 4,
+            name: "Клавиатура проводная Dark Project KD83A",
+            price: 9999,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("periphery/7.webp")
+        },
+        {
+            id: 44,
+            category: 4,
+            name: "Клавиатура проводная ARDOR GAMING Blade PRO",
+            price: 4199,
+            sale_price: null,
+            rating: 4.3,
+            image: getImagePath("periphery/8.webp")
+        },
+        {
+            id: 45,
+            category: 4,
+            name: "Мышь проводная ARDOR GAMING Fury [ARD-FURY3327-BK]",
+            price: 1699,
+            sale_price: null,
+            rating: 4.2,
+            image: getImagePath("periphery/9.webp")
+        },
+        {
+            id: 46,
+            category: 4,
+            name: "Мышь беспроводная/проводная LAMZU Atlantis черный",
+            price: 5699,
+            sale_price: null,
+            rating: 4.4,
+            image: getImagePath("periphery/10.webp")
+        },
+        {
+            id: 47,
+            category: 4,
+            name: "Мышь беспроводная Logitech G PRO X SUPERLIGHT",
+            price: 12699,
+            sale_price: null,
+            rating: 4.6,
+            image: getImagePath("periphery/11.webp")
+        },
+        {
+            id: 48,
+            category: 4,
+            name: "Мышь беспроводная/проводная LAMZU Atlantis белый",
+            price: 6699,
+            sale_price: null,
+            rating: 4.5,
+            image: getImagePath("periphery/12.webp")
         }
     ],
     categories: [
